a69b47f928652319388ffde6029f729fdf339cd8
congenesy <congnvc@gmail.com>
2016-03-31 20:02:44 +0700

    1
    1

---
+++ Enesy/EnesyCAD/DEMO/CE/CEForm.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Drawing;
+using System.Globalization;
+using System.Windows.Forms;
+
+using MetroFramework.Forms;
+using System.Data;
+
+namespace Enesy.EnesyCAD.DEMO.CE
+{
+    public partial class CEForm : MetroForm
+    {
+        public CEForm()
+        {
+            InitializeComponent();
+            this.StyleManager = metroStyleManager1;
+        }
+    }
+}
---
+++ Enesy/EnesyCAD/DEMO/CE/Commands.cs
@@ -0,0 +1,32 @@
+﻿using Autodesk.AutoCAD.Runtime;
+
+using System.Runtime.InteropServices;
+using Autodesk.AutoCAD.ApplicationServices;
+using Autodesk.AutoCAD.EditorInput;
+using acApp = Autodesk.AutoCAD.ApplicationServices.Application;
+
+using Enesy.EnesyCAD.DEMO.CE;
+
+namespace Enesy.EnesyCAD.CE
+{
+    public class Command
+    {
+        private CEForm cmdMngDia = null;
+
+        [EnesyCAD.Runtime.EnesyCADCommandMethod("CEF",
+            "Manager",
+            "Management all Enesy commands",
+            "enesy.vn",
+            "quandt@enesy.vn",
+            Enesy.Page.CadYoutube
+            )]
+        public void ListUserCommandDialog()
+        {
+            if (cmdMngDia == null)
+            {
+                this.cmdMngDia = new CEForm();
+            }
+            Autodesk.AutoCAD.ApplicationServices.Application.ShowModalDialog(cmdMngDia);
+        }
+    }
+}
